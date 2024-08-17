aea6a41c4859ddffddc7f406bb39ad78794c96d1
saytreep <treep.me@yandex.ru>
2024-08-17 11:47:54 +0300

    :)

--- build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/Users/ikard/Desktop/hse")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/ikard/Desktop/hse")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/ikard/Desktop/hse/build")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
